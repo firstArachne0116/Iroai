350722d003fb92412e5b032e5a5893eb735a2930
Arachne <firstArachne0116@gmail.com>
2021-02-27 06:22:10 +1000

    android update for publish

--- SmartApp/SmartApp.Android/MainActivity.cs
+++ SmartApp/SmartApp.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using Xamarin.Forms;
 
 namespace SmartApp.Droid
 {
-    [Activity(Label = "SmartApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "iroai", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
